bc47532dc642e2c538de7fadc5bfe129e21ce7cb
UGG-Favo <46539984+UGG-Favo@users.noreply.github.com>
2020-05-23 10:31:20 +0200

    Create Room.java

---
+++ src/Room.java
@@ -0,0 +1,13 @@
+public class Room {
+  boolean isNallowed;
+  boolean isSallowed;
+  boolean isEallowed;
+  boolean isWallowed;
+  
+  public Room (boolean n, boolean s, boolean e, boolean w) {
+    isNallowed = n;
+    isSallowed = s;
+    isEallowed = e;
+    isWallowed = w;  
+  }
+}
